a1c4a95285ff5b216f821834f30f900e076b6984
sumdx <sumdas74@gmail.com>
2025-01-15 10:59:53 -0800

    feat(footer) : footer component added to all page

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,17 +1,24 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../Components/NavBar";
+import Footer from "../Components/Footer";
+
 
 
 const Root = () => {
     return (
-        <div className='relative font-poppins bg-gray-50  dark:bg-gray-900 flex flex-col min-h-screen'>
+        
+        <div>
+            <div className='relative font-poppins bg-gray-50  dark:bg-gray-900 flex flex-col min-h-screen'>
         <NavBar className="absolute top-0  mx-auto z-20 bg-transparent"></NavBar>
         <div className='flex-grow '>
         <Outlet></Outlet>
+        
         </div>
         
-        
-    </div>
+        <Footer></Footer>
+         </div>
+   
+        </div>
     );
 };
 
